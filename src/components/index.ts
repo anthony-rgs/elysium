19bfbe4d15835ecfc3e8838c8566ee88dc7c3838
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-07-24 03:07:30 +0200

    feat: add import file for components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import ButtonPrimary from "./Buttons/ButtonPrimary";
+import ButtonSecondary from "./Buttons/ButtonSecondary";
+import ButtonTertiary from "./Buttons/ButtonTertiary";
+
+import IconContainer from "./IconContainer";
+
+export { ButtonPrimary, ButtonSecondary, ButtonTertiary, IconContainer };
