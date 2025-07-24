6d94d61c826f181fc2495f5a49991f65bcbf7b9f
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-07-24 03:11:08 +0200

    chore: add all svgs

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,21 @@
+import BrowseIcon from "./BrowseIcon";
+import CrossIcon from "./CrossIcon";
+import DownloadIcon from "./DownloadIcon";
+import FacebookIcon from "./FacebookIcon";
+import InstagramIcon from "./InstagramIcon";
+import SearchIcon from "./SearchIcon";
+import TwitterIcon from "./TwitterIcon";
+import HomeIcon from "./HomeIcon";
+import WorldIcon from "./WorldIcon";
+
+export {
+  BrowseIcon,
+  CrossIcon,
+  DownloadIcon,
+  FacebookIcon,
+  HomeIcon,
+  InstagramIcon,
+  SearchIcon,
+  TwitterIcon,
+  WorldIcon,
+};
