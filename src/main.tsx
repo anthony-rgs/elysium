0e63f5fba6199256937884d2513ee76d17e5c953
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-07-24 03:24:27 +0200

    refacto: some files and add fonts in theme.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./theme.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="bg-black text-white font-circular-book h-screen">
+      <App />
+    </div>
+  </StrictMode>
+);
