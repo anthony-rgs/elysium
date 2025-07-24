47fc3898e991c4eeee144c2be65c985d38f1ffd4
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-07-24 03:05:51 +0200

    feat: add primary button

---
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -0,0 +1,29 @@
+type Props = {
+  label: string;
+  link: string;
+  size: "medium" | "large";
+};
+
+export default function ButtonPrimary({ label, link, size }: Props) {
+  const baseStyles =
+    "bg-white text-black font-circular-bold rounded-max cursor-pointer transition-transform transform-gpu duration-[90ms] antialiased hover:scale-[1.04] hover:bg-white-hover active:scale-[1] active:bg-white-active";
+
+  const sizeStyles =
+    size === "large"
+      ? "py-2 px-8 text-base h-12 tracking-[-0.0133em]"
+      : "py-1 px-4 text-[0.813rem] h-8 tracking-[-0.0053em] sm:text-sm sm:tracking-[-0.005em]";
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+    >
+      <button
+        className={`${baseStyles} ${sizeStyles}`}
+        type="button"
+      >
+        {label}
+      </button>
+    </a>
+  );
+}
