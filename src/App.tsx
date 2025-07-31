1808f2d8128a50e3e2565b373bd0351924acc3f1
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-01 01:55:04 +0200

    feat: provider and layout component on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import React from "react";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { routes } from "@/utils/routes";
+import { routes } from "@/utils";
+import { Layout } from "@/components";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={element}
-          />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              {routes.map(({ path, element }) => (
+                <Route
+                  key={path}
+                  path={path}
+                  element={element}
+                />
+              ))}
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
   );
 }
