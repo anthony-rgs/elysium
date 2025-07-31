3be2feb220142eadf124daf745564a6027b20a5d
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-01 01:47:23 +0200

    refactor: link button component

--- src/components/Buttons/LinkButton.tsx
+++ src/components/Buttons/LinkButton.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 type Props = {
+  blank: boolean;
   color: "grey" | "white";
   font: "light" | "book" | "bold";
   label: string;
@@ -8,7 +9,14 @@ type Props = {
   size: "extra_small" | "small" | "medium" | "large" | "extra_large";
 };
 
-export default function LinkButton({ color, font, label, link, size }: Props) {
+export default function LinkButton({
+  blank,
+  color,
+  font,
+  label,
+  link,
+  size,
+}: Props) {
   const fontClassName = `font-circular-${font}`;
 
   const sizeClassNames = {
@@ -19,12 +27,34 @@ export default function LinkButton({ color, font, label, link, size }: Props) {
     extra_large: "text-2xl sm:text-[32px] tracking-[-0.02em]",
   }[size];
 
-  return (
-    <Link
-      to={link}
-      className={`${fontClassName} ${sizeClassNames} text-${color} w-fit hover:text-white hover:underline`}
-    >
-      {label}
-    </Link>
-  );
+  const baseClassNames = `${fontClassName} ${sizeClassNames} text-${color} w-fit hover:text-white hover:underline`;
+
+  const blankComponent = () => {
+    return (
+      <a
+        className={baseClassNames}
+        href={link}
+        target="_blank"
+      >
+        {label}
+      </a>
+    );
+  };
+
+  const linkComponnet = () => {
+    return (
+      <Link
+        className={baseClassNames}
+        to={link}
+      >
+        {label}
+      </Link>
+    );
+  };
+
+  if (blank) {
+    return blankComponent();
+  } else {
+    return linkComponnet();
+  }
 }
