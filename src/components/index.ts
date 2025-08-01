d8b691ee3ea921cd3f9df865bff0e09aba8428c0
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-01 02:14:36 +0200

    feat: add all new components to an index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,61 @@
-import ButtonPrimary from "./Buttons/ButtonPrimary";
-import ButtonSecondary from "./Buttons/ButtonSecondary";
-import ButtonTertiary from "./Buttons/ButtonTertiary";
+// Buttons
+import LinkButton from "./Buttons/LinkButton";
+import PrimaryButton from "./Buttons/PrimaryButton";
+import SecondaryButton from "./Buttons/SecondaryButton";
+import TabButton from "./Buttons/TabButton";
+import TertiaryButton from "./Buttons/TertiaryButton";
 
+// Cards
+import InformationCard from "./Cards/InformationCard";
+
+// Footer
+import FooterColumn from "./Footer/FooterColumn";
+import FooterPagesContainer from "./Footer/FooterPagesContainer";
+
+// Search Bars
+import SearchBarNav from "./SearchBars/SearchBarNav";
+
+// Other
+import Divider from "./Divider";
+import DynamicTitle from "./DynamicTitle";
 import IconContainer from "./IconContainer";
+import InformationsContainer from "./InformationsContainer";
+import Layout from "./Layout";
+import MusicPageHeader from "./MusicPageHeader";
+import PlayButton from "./PlayButton";
+import SpotifyPlayerContainer from "./SpotifyPlayerContainer";
+import NavBar from "./NavBar";
+import TabsBar from "./TabsBar";
+import TrackRow from "./TrackRow";
 
-export { ButtonPrimary, ButtonSecondary, ButtonTertiary, IconContainer };
+export {
+  // Buttons
+  LinkButton,
+  PrimaryButton,
+  SecondaryButton,
+  TabButton,
+  TertiaryButton,
+
+  // Cards
+  InformationCard,
+
+  // Footer
+  FooterColumn,
+  FooterPagesContainer,
+
+  // Search Bars
+  SearchBarNav,
+
+  // Other
+  Divider,
+  DynamicTitle,
+  IconContainer,
+  InformationsContainer,
+  Layout,
+  MusicPageHeader,
+  PlayButton,
+  SpotifyPlayerContainer,
+  NavBar,
+  TabsBar,
+  TrackRow,
+};
