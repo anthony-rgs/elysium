c171dc920a2cae5c16a2947ecc0532dcc7385f94
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-08 00:31:01 +0200

    feat: add search bar tracks component

---
+++ src/components/SearchBars/SearchBarTracks.tsx
@@ -0,0 +1,128 @@
+import { IconContainer } from "@/components";
+import { CrossIcon, SearchIcon } from "@/assets/icons";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  clearFilterQuery,
+  setFilterQuery,
+} from "@/store/tracks/filterTracks.slice";
+import { setCurrentPage } from "@/store";
+
+export default function SearchBarTracks() {
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // Clear input
+  const handleClearInput = () => {
+    dispatch(setCurrentPage(1));
+    inputRef.current?.focus();
+    dispatch(clearFilterQuery());
+    setInputValue("");
+  };
+
+  // At the opening, focus input
+  useEffect(() => {
+    if (isOpen) {
+      inputRef.current?.focus();
+    }
+  }, [isOpen]);
+
+  // Close the search bar when the user clicks outside it and no inputValue
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node) &&
+        !inputValue
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [inputValue]);
+
+  // When inputValue change -> Update query value and reset current page number
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      dispatch(setFilterQuery(inputValue));
+      dispatch(setCurrentPage(1));
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [inputValue]);
+
+  return (
+    <div
+      className="flex items-center"
+      ref={containerRef}
+    >
+      {/* Open -> Search bar */}
+      <div
+        className={`flex gap-2 items-center mr-[-32px] transition-all duration-100 ease-in-out h-8 bg-elevated-highlight 
+          rounded-sm overflow-hidden ${isOpen ? "p-2 w-auto" : "w-0"}`}
+      >
+        <IconContainer
+          color="grey"
+          icon={<SearchIcon />}
+          size="small"
+          tooltipPosition="top"
+          tooltipText="Search in playlist"
+        />
+
+        <input
+          className="text-sm outline-none text-grey w-full max-w-[280px] placeholder-grey z-1"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Search in playlist"
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+        />
+
+        <div
+          className="flex items-center w-4 h-4 z-1"
+          onClick={handleClearInput}
+        >
+          {inputValue && (
+            <IconContainer
+              color="grey"
+              icon={<CrossIcon />}
+              size="small"
+              tooltipPosition="top"
+              tooltipText="Clear search field"
+            />
+          )}
+        </div>
+      </div>
+
+      {/* Close -> button open */}
+      <div
+        className={`group flex gap-2 cursor-pointer items-center transition-all ease-in-out h-8 p-2 hover:bg-elevated-highlight 
+          rounded-full ${
+            isOpen ? "opacity-0 duration-100" : "opacity-100 duration-400"
+          }`}
+        onClick={() => setIsOpen(true)}
+        ref={containerRef}
+      >
+        <div className="h-4 w-4">
+          <IconContainer
+            color="grey"
+            icon={<SearchIcon />}
+            size="small"
+            tooltipPosition="top"
+            tooltipText={!isOpen ? "Search in playlist" : undefined}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
