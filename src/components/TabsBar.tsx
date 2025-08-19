cdd9071a238f6f748e9ba4a0dd0c46cc77d974e9
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-19 10:22:03 +0200

    feat: add a scrollToTop button on tabsBar component

--- src/components/TabsBar.tsx
+++ src/components/TabsBar.tsx
@@ -1,20 +1,26 @@
 import { useLocation } from "react-router-dom";
 import { tabs } from "@/utils";
 import { TabButton } from "@/components";
-import { useDispatch } from "react-redux";
-import { useEffect, useRef, useState } from "react";
-import { setCurrentPage, type RootState } from "@/store";
+import { useContext, useEffect, useRef, useState } from "react";
+import { type RootState } from "@/store";
 import { useSelector } from "react-redux";
+import { ScrollParentContext } from "@/contexts";
 
 export default function TabsBar() {
-  const dispatch = useDispatch();
   const location = useLocation();
-
   const [showText, setShowText] = useState(false);
   const topSentinelRef = useRef<HTMLDivElement | null>(null);
   const pageTitle = useSelector((state: RootState) => state.pageTitle.title);
+  const scrollParent = useContext(ScrollParentContext);
 
   const currentPage = location.pathname;
+
+  // Scroll to page top
+  const handleScrollToTop = () => {
+    if (showText) {
+      scrollParent?.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
 
   // Observer for display title
   useEffect(() => {
@@ -35,11 +41,6 @@ export default function TabsBar() {
     return () => observer.disconnect();
   }, []);
 
-  // Reset "current page" every time page change
-  useEffect(() => {
-    dispatch(setCurrentPage(1));
-  }, [location]);
-
   return (
     <>
       {/* Invisible top sentinel for IntersectionObserver (triggers show/hide text after scrolling 224px) */}
@@ -48,7 +49,7 @@ export default function TabsBar() {
         className="absolute top-56 "
       />
 
-      <div className="sticky top-0 flex gap-3 justify-between items-center rounded-t-lg bg-container px-5 py-3 z-3">
+      <div className="sticky top-0 flex gap-3 justify-between items-center rounded-t-lg bg-container px-5 py-3 z-9">
         <div className="flex gap-2">
           {tabs.map((tab) => (
             <TabButton
@@ -60,13 +61,16 @@ export default function TabsBar() {
           ))}
         </div>
 
-        <p
-          className={`font-circular-bold text-[21px] transition-opacity delay-100 truncate ${
-            showText ? "opacity-100" : "opacity-0"
-          }`}
+        <div
+          className={`${
+            showText ? "opacity-100 cursor-pointer" : "opacity-0"
+          } group`}
+          onClick={() => handleScrollToTop()}
         >
-          {pageTitle}
-        </p>
+          <p className="font-circular-bold text-[21px] transition-opacity delay-100 truncate group-hover:underline">
+            {pageTitle}
+          </p>
+        </div>
       </div>
     </>
   );
