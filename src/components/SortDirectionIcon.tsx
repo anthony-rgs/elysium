c347b0c093f25a227440a11b7a95665d10054a63
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-08-19 10:11:52 +0200

    feat: add a sort direction icon component

---
+++ src/components/SortDirectionIcon.tsx
@@ -0,0 +1,15 @@
+import { SortIcon } from "@/assets/icons";
+
+type Props = {
+  direction: "asc" | "desc";
+};
+
+export default function SortDirectionIcon({ direction }: Props) {
+  return (
+    <span className={`${direction === "asc" ? "rotate-O" : "rotate-180"}`}>
+      <div className={`flex items-center gap-2 h-4 w-4 text-green `}>
+        <SortIcon />
+      </div>
+    </span>
+  );
+}
