4d279962c53f1398c038711d9b0eab4e38382661
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-09-21 18:17:20 +0200

    feat: update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,18 +14,37 @@ import {
   fetchTitles,
   fetchTracksMeta,
   type AppDispatch,
+  type RootState,
 } from "@/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 
 import { ScrollParentContext } from "@/contexts";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import useElementWidth from "@/hooks/useElementWidth";
+import { Mobile } from "@/pages";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: Props) {
+  // Calculate box width
+  const boxRef = useRef<HTMLDivElement>(null);
+  const { width } = useElementWidth(boxRef);
+
+  // Displaying a page not found error when there is an error in artist or album fetch
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const artistError = useSelector((state: RootState) => state.artists.error);
+  const albumError = useSelector((state: RootState) => state.albums.error);
+
+  useEffect(() => {
+    if (artistError || albumError) {
+      navigate("/notFound");
+    }
+  }, [artistError, albumError]);
 
   // Init all datas
   useEffect(() => {
@@ -44,8 +63,20 @@ export default function Layout({ children }: Props) {
     if (el) setScrollParent(el);
   }, []);
 
+  // If width under 980 -> mobile page
+  if (width < 980) {
+    return (
+      <div ref={boxRef}>
+        <Mobile />
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col gap-2 h-screen bg-black text-white font-circular-light">
+    <div
+      className="flex flex-col gap-2 h-screen bg-black text-white font-circular-light"
+      ref={boxRef}
+    >
       <NavBar />
 
       <div className="flex flex-1 h-full pt-16 p-2">
